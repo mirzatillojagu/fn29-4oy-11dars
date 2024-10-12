7ae5c234edf5f4cec8f4c88575ef916d70f70d4e
mirzatillojagu <imirzatillo0@gmail.com>
2024-10-12 17:45:58 +0500

    vazifa

---
+++ js/index.js
@@ -0,0 +1,223 @@
+///////////////Oson masalalar//////////////
+
+// 1
+// function kvadrat (arg){
+//     return arg*2
+//    }
+//    console.log(kvadrat(2));
+
+// // 2
+// function num(arg1, arg2){
+//     return arg1 + arg2
+// }
+// console.log(num(2,5));
+
+// 3
+// function a(arg){
+//     return(`slaom ${arg}`)
+// }
+// console.log(a('bobr'));
+
+// 4
+// function num(arg1,arg2){
+//     let max=0;
+//     if (arg1 > arg2) {
+//         max = arg1
+//     }else{
+//         max = arg2
+//     }
+
+//     return max
+// }
+// console.log(num(2,1));
+
+// 5
+
+// function num(arg) {
+//   let sum = 0;
+//   for (let i = 0; i < arg.length; i++) {
+//     sum += arg[i];
+//   }
+
+//   return sum;
+// }
+// let arr = [1, 2, 3, 4];
+// console.log(num(arr));
+
+// 6
+// function num(arg){
+//     if (arg > 0) {
+//         return "musbat"
+//     }else{
+//         return "musbat emas"
+//     }
+// }
+
+// console.log(num(2));
+
+// 7
+// function num(arg){
+//     return arg.length;
+// }
+// console.log(num('salom'));
+
+// 8
+// function num(arg1,arg2){
+//     return arg1 + arg2
+// }
+// console.log(num('salom', 'xayr'));
+
+// 9
+// function num(arg1,arg2){
+//     let min=0;
+//     if (arg1 > arg2) {
+//         min = arg2
+//     }else{
+//         min = arg1
+//     }
+
+//     return min
+// }
+// console.log(num(2,1));
+
+// 10
+// function num(arg){
+//     let res = arg.split("").reverse().join('')
+//     return res;
+// }
+// console.log(num('bobr'));
+
+////////////////////Massiv va funksiyalar/////////////////
+
+// 1
+// function num(arg){
+//     for (let i = 0; i < arg.length; i++) {
+//         return  arg;
+//     }
+// }
+// let arr =[1, 2, 3, 4, 5];
+// console.log(num(arr));
+
+// 2
+// function num(arg){
+//     let max= Math.max(...arg);
+//     return max
+
+// }
+// let arr = [1, 2, 3, 4, 5, 6]
+// console.log(num(arr));
+
+// 3
+// function num(arg){
+//     for (let i = 0; i < arg.length; i++) {
+//         return  arg.length;
+//     }
+// }
+// let arr =[1, 2, 3, 4, 5];
+// console.log(num(arr));
+
+// 4
+// function num(arg){
+//     let sum=0;
+//     for (let i = 0; i < arg.length; i++) {
+//         sum += arg[i]
+//     }
+//     return sum
+// }
+// let arr =[1, 2, 3, 4, 5];
+// console.log(num(arr));
+
+// 5
+// function num(arg){
+//     let count=0;
+//     for (let i = 0; i < arg.length; i++) {
+//         if(arr[i] === elemt){
+//             count++
+//         }
+//     }
+//     return count
+// }
+// let arr =[1, 2, 3, 4, 5,5];
+// let elemt =5;
+// console.log(num(arr,elemt));
+
+// 6
+// function num(arg){
+//     for(let i = 0; i < arg.length; i++){
+//         if(arg[i] % 2 == 1){
+//             res.push(arg[i])
+//         }
+//     }
+//     return res
+// }
+// let arr =[1,2,3,4,5,6,7,8,9,10]
+// let res =[]
+// console.log(num(arr));
+
+// 8
+// function num(arg){
+//     for(let i = 0; i < arg.length; i++){
+//         if(arg[i] < 0){
+//             res.push(arg[i])
+//         }
+//     }
+//     return res
+// }
+// let arr =[1, -2, 3, -4, 5,-6,7,8,-9,-10]
+// let res =[]
+// console.log(num(arr));
+
+// 9
+// function num(arg) {
+//   arg.pop()
+
+//   return arg
+// }
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let res = [];
+// console.log(num(arr));
+
+// 10
+// function num(arg) {
+//   arg.push(69)
+
+//   return arg
+// }
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let res = [];
+// console.log(num(arr));
+
+// 11
+// function num(arg1,arg2) {
+//    return arg1.concat(arg2)
+// }
+
+// let arr1 = [1, 2, 3, 4, 5, 6];
+// let arr2 = [7, 8, 9, 10, 11, 12]
+
+// console.log(num(arr1,arr2));
+
+// 12
+// function num(arg) {
+//     let res = arg.map(num => num * num)
+//   return res
+// }
+// let arr = [1, 2, 3, 4, 5]
+// console.log(num(arr));
+
+
+// 13
+// let arr =[1, 2, 3, 4, 5, 6, 7, 8, 9]
+// let res =[]
+// function num(arr){
+//     for(let i = 0; i < arr.length; i++){
+//         if(arr[i] % 2 != 0){
+//             res.push(arr[i])
+//         }
+//     }
+//     return res
+// }
+// console.log(num(arr));
+
+
+// 14
